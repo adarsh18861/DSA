b1bb402b828e28747ae140f15d88aa971a29f888
Adarsh Tiwari <at3250289@gmail.com>
2026-08-20 00:33:38 +0530

    Time: 51 ms (89.24%) | Memory: 262.7 MB (90.84%) - LeetSync

--- 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
+++ 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
@@ -4,51 +4,54 @@
 using namespace std;
 
 class Solution {
+    vector<int> tree;
+    
+    void build(int node, int l, int r, const vector<int>& heights) {
+        if (l == r) {
+            tree[node] = heights[l];
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        build(2 * node + 1, l, mid, heights);
+        build(2 * node + 2, mid + 1, r, heights);
+        tree[node] = max(tree[2 * node + 1], tree[2 * node + 2]);
+    }
+    
+    int query(int node, int l, int r, int ql, int qr, int target, const vector<int>& heights) {
+        if (l > qr || r < ql || tree[node] <= target) {
+            return -1;
+        }
+        if (l == r) {
+            return l;
+        }
+        int mid = l + (r - l) / 2;
+        int left_res = query(2 * node + 1, l, mid, ql, qr, target, heights);
+        if (left_res != -1) {
+            return left_res;
+        }
+        return query(2 * node + 2, mid + 1, r, ql, qr, target, heights);
+    }
+
 public:
     vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
         int n = heights.size();
-        int q = queries.size();
-        vector<int> ans(q, -1);
-        vector<vector<pair<int, int>>> store(n);
-
-        for (int i = 0; i < q; ++i) {
-            int a = queries[i][0];
-            int b = queries[i][1];
+        tree.assign(4 * n, 0);
+        build(0, 0, n - 1, heights);
+        
+        vector<int> ans;
+        ans.reserve(queries.size());
+        
+        for (const auto& q : queries) {
+            int a = q[0], b = q[1];
             if (a > b) swap(a, b);
             
             if (a == b || heights[a] < heights[b]) {
-                ans[i] = b;
+                ans.push_back(b);
             } else {
-                store[b].push_back({heights[a], i});
+                int res = query(0, 0, n - 1, b + 1, n - 1, heights[a], heights);
+                ans.push_back(res);
             }
         }
-
-        vector<int> st;
-        for (int i = n - 1; i >= 0; --i) {
-            for (auto& q_pair : store[i]) {
-                int target = q_pair.first;
-                int q_idx = q_pair.second;
-                
-                int low = 0, high = (int)st.size() - 1;
-                int res = -1;
-                while (low <= high) {
-                    int mid = low + (high - low) / 2;
-                    if (heights[st[mid]] > target) {
-                        res = st[mid];
-                        low = mid + 1;
-                    } else {
-                        high = mid - 1;
-                    }
-                }
-                ans[q_idx] = res;
-            }
-
-            while (!st.empty() && heights[st.back()] <= heights[i]) {
-                st.pop_back();
-            }
-            st.push_back(i);
-        }
-
         return ans;
     }
 };
