ada39786166a17d50ef8b429e73b92f2c3f1019a
Adarsh Tiwari <at3250289@gmail.com>
2026-08-26 14:45:39 +0530

    Time: 98 ms (36.21%) | Memory: 89.5 MB (35.50%) - LeetSync

---
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        
+        // dp[i][j] stores length of common subarray ending at nums1[i-1] and nums2[j-1]
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        int maxLen = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    maxLen = max(maxLen, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        
+        return maxLen;
+    }
+};
